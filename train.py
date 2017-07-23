e7e874dd6356317c70a7547fbad4003235a8e996
antoine_MSK <antoine.rivail@meinespielzeugkiste.de>
2017-07-24 00:09:12 +0200

    save

--- train.py
+++ train.py
@@ -1,22 +1,41 @@
 import numpy as np
 from scipy.signal import convolve2d
+from sklearn.datasets import load_digits
 
+
+def load_mnist_data():
+    data = load_digits(return_X_y=True)
+    x_train = np.zeros((1500,8,8))
+    y_train = np.zeros((1500,10))
+    
+    for i,im in enumerate(data[0][:1500]):
+        x_train[i] = im.reshape((8,8))
+        y_train[i][data[1][i]] = 1
+
+    x_test = np.zeros((data[0].shape[0]-1500,8,8))
+    y_test = np.zeros((data[0].shape[0]-1500,10))
+    for i,im in enumerate(data[0][1500:]):
+        x_test[i] = im.reshape((8,8))
+        y_test[i][data[1][i+1500]] = 1
+
+    return x_train,y_train,x_test,y_test
+    
 def initialize_weights(path="default"):
     weights = []
     if path != "default":
         #load weights
         print('hello')
     else:
-        alpha0 = 0.001
-        weights.append(alpha0*np.ones((16,3,3)))
-        weights.append(alpha0*np.ones((16,3,3)))
-        weights.append(alpha0*np.ones((32,6400)))
-        weights.append(alpha0*np.ones((10,32)))
+        alpha0 = 0.05
+        weights.append(alpha0*np.random.sample((16,3,3))-alpha0/2)
+        weights.append(alpha0*np.random.sample((16,3,3))-alpha0/2)
+        weights.append([alpha0*np.random.sample((1024,32))-0.5*alpha0,alpha0*np.random.sample(32)-0.5*alpha0])
+        weights.append([alpha0*np.random.sample((32,10))-0.5*alpha0,alpha0*np.random.sample(10)-0.5*alpha0])
 
     return weights
 
 def relu(x):
-    activate = lambda x: x if x>=0 else 0
+    activate = lambda x: x if x>=0 else -0.01*x
     relufunc = np.vectorize(activate)
     return relufunc(x)
     
@@ -44,24 +63,31 @@ def conv2d(x,filters):
 
 def run_network(x,weights):
     #architecture
-    print('run network')
+    #print('run network')
     output = relu(conv2d(x,weights[0]))
-    print(output.shape)
+    #print(output.shape)
     output = relu(conv2d(output,weights[1]))
-    print(output.shape)
+    #print(output.shape)
     output = output.flatten()
-    print(output.shape)
-    output = relu(np.dot(weights[2],output))
-    print(output.shape)
-    output = softmax(np.dot(weights[3],output))
-    print(output.shape)
+    #print(output.shape)
+    output = relu(np.dot(output,weights[2][0])+weights[2][1])
+    #print(output.shape)
+    output = softmax(np.dot(output,weights[3][0])+weights[3][1])
+    #print(output.shape)
     return output
          
 def d_relu(x):
-    activate = lambda x: 1 if x>=0 else 0
+    activate = lambda x: 1 if x>=0 else -0.01
     relufunc = np.vectorize(activate)
     return relufunc(x)       
 
+def evaluate(data,labels,weights):
+    ct_error=0.
+    for x,y in zip(data,labels):
+        x_pred = np.argmax(run_network(x,weights))
+        if x_pred==np.argmax(y):
+            ct_error+=1.
+    print('Evaluation accuracy: '+str(ct_error/len(data)))
 #def d_softmax(x):
     
 def flip_filter(w):
@@ -78,68 +104,97 @@ def compute_delta(d,y,fshape):
                             for j in range(y.shape[2]-fshape[2]):
                                 delta[c,a,b] +=d[c,i,j] *y[k,i+a,j+b]
                 else:
-                    for i in range(y.shape[0]-fshape[0]):
-                        for j in range(y.shape[1]-fshape[1]):
+                    for i in range(y.shape[0]-fshape[1]):
+                        for j in range(y.shape[1]-fshape[2]):
                             delta[c,a,b] +=d[c,i,j] *y[i+a,j+b]
                     
     return delta
+def cross_entropy(y_true,y_predicted):
+    return -1.*np.sum(y_true*np.log(y_predicted))
+        
+def upgrade_network(x_batch,y_batch,weights,lrate):
+    #print(x_batch.shape)
+    delta0 = np.zeros(weights[0].shape)
+    delta1 = np.zeros(weights[1].shape)
+    delta2 = [np.zeros(weights[2][0].shape),np.zeros(weights[2][1].shape)]
+    delta3 = [np.zeros(weights[3][0].shape),np.zeros(weights[3][1].shape)]
+    LOSS=0.
+    #print('todo')
+    for x,y in zip(x_batch,y_batch):
+        #conv1
+        x0 = conv2d(x,weights[0])
+        y0 = relu(x0)
+        #print('c1 '+str(np.max(y0)))
+        s0 = d_relu(x0)
+        
+        #conv2
+        x1 = conv2d(y0,weights[1])
+        y1 = relu(x1)
+        #print('c2 '+str(np.max(y1)))
+        s1 = d_relu(x1)
+        
+        y1_flatten = y1.flatten()
+        
+        #dense
+        x2 = np.dot(y1_flatten,weights[2][0])+weights[2][1]
+        y2 = relu(x2)
+        s2 = d_relu(x2)
+        #print('d1 '+str(np.max(y2)))
+        
+        #softmax
+        x3 = np.dot(y2,weights[3][0])+weights[3][1]
+        y3 = softmax(x3)
+        s3 = d_relu(x3)
+        #print('d2 '+str(np.max(y3)))
+        #print('weights')
+        #for i in range(4):
+            #print(np.max(weights[i]))
+        LOSS += cross_entropy(y,y3)
+        
+        
+        #back propagation
+        d3 = y-y3
+        #print(d3)
+        delta3[0] +=  np.dot(np.array([y2]).T,np.array([d3]))
+        delta3[1]+=d3
+        gamma2 = np.dot(d3,weights[3][0].T)
+        
+        d2 = s2 * gamma2
+        delta2[1]+=d2
+        delta2[0] += np.dot(np.array([y1_flatten]).T,np.array([d2]))
+        #print('shape '+str(delta2.shape))
+        gamma1_flatten = np.dot(d2,weights[2][0].T)
+        
+        #unflatten    
+        gamma1 = gamma1_flatten.reshape(y1.shape)
+             
+        d1 = s1*gamma1
     
-def upgrade_network(x,y,weights,lrate):
-    print('todo')
-    #conv1
-    x0 = conv2d(x,weights[0])
-    y0 = relu(x0)
-    s0 = d_relu(x0)
+        delta1 += compute_delta(d1,y0,weights[1].shape)
+        gamma0 = np.sum([conv2d(d1[c],flip_filter(weights[1][c])) for c in range(weights[1].shape[0])])
+        
+        d0 = s0*gamma0
+        delta0 += compute_delta(d0,x,weights[0].shape)
+        #print(np.max(delta0))
+      
+        #print(np.max(delta3))
+    LOSS/=len(x_batch)
+    #print('LOSS >> '+str(LOSS))
+    delta0/=len(x_batch)
+    delta1/=len(x_batch)
+    delta2[0]/=len(x_batch)
+    delta2[1]/=len(x_batch)
+    delta3[0]/=len(x_batch)
+    delta3[1]/=len(x_batch)
     
-    #conv2
-    x1 = conv2d(y0,weights[1])
-    y1 = relu(x1)
-    s1 = d_relu(x1)
-    
-    y1_flatten = y1.flatten()
-    
-    #dense
-    x2 = np.dot(weights[2],y1_flatten)
-    y2 = relu(x2)
-    s2 = d_relu(x2)
-    
-    #softmax
-    x3 = np.dot(weights[3],y2)
-    y3 = relu(x3)
-    s3 = d_relu(x3)
-    
-    #back propagation
-    d3 = y3 - y
-    delta3 = np.dot(y3.T,d3)
-    gamma2 = np.dot(y2.T,delta3)
-    
-    d2 = s2 * gamma2
-    delta2 = np.dot(y2.T,d2)
-    gamma1_flatten = np.dot(y1.T,delta2)
-    
-    #unflatten    
-    gamma1 = gamma1_flatten.reshape(y1.shape)
-         
-    d1 = s1*gamma1
-    for k in range(weights[0].shape[0]):
-        for c in range(weights[1].shape[0]):
-            print(k,c)
-            print(d1[0,:d1.shape[1]-weights[1].shape[1]+1,:d1.shape[2]-weights[1].shape[2]+1].shape)
-            print(y0[0].shape)
-            print('<>'*10)
-    delta1 = compute_delta(d1,y0,weights[1].shape)
-    gamma0 = np.sum([conv2d(d1[c],flip_filter(weights[1][c])) for c in range(weights[1].shape[0])])
-    
-    d0 = s0*gamma0
-    delta0 = compute_delta(d0,x,weights[0].shape)
-  
-    print(delta0.shape)
     weights[0]+=lrate*delta0
     weights[1]+=lrate*delta1    
-    weights[2]+=lrate*delta2    
-    weights[3]+=lrate*delta3
-
-    return weights 
+    weights[2][0]+=lrate*delta2[0]
+    weights[2][1]+=lrate*delta2[1]
+    weights[3][0]+=lrate*delta3[0]
+    weights[3][1]+=lrate*delta3[1]
+    #print(np.max(delta3[0]),np.max(delta0),np.max(delta1),np.max(delta2[0]))
+    return weights,LOSS 
 
     
     
@@ -158,7 +213,7 @@ if __name__ =='__main__':
     print(relu(test))
     print(relu(test2))
     
-    test_img = np.zeros((20,20))
+    test_img = np.zeros((8,8))
     
     weights = initialize_weights()
     for i in range(1):
@@ -173,7 +228,25 @@ if __name__ =='__main__':
     print(test_img.shape)
     print(y.shape)
     
-    weights = upgrade_network(test_img,y,weights,lrate =0.001)
-
-
+    #weights = upgrade_network(test_img,y,weights,lrate =0.001)
+    
+    x_train,y_train,x_test,y_test = load_mnist_data()
+    weights = initialize_weights()
+    lrate =0.1
+    epochs = 50
+    decay = lrate/epochs
+    print(y_train[0])
+    print('LOSS >> '+str(cross_entropy(y_train[0],result)))
+    for e in range(epochs):
+        print('<>'*10)
+        print('epoch '+ str(e))
+        for i in range(46):
+            #print('BATCH '+str(i))
+            weights,LOSS = upgrade_network(x_train[32*i:32*(i+1)],y_train[32*i:32*(i+1)],weights,lrate=lrate)
+            if LOSS>100:
+                print('Loss explosion')
+                break
+        print('LOSS >> '+str(LOSS))
+        evaluate(x_test,y_test,weights)
+        lrate-=decay
 
