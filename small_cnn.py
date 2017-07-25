0476407c21f74281900380fa1a517e33a761779a
antoine_MSK <antoine.rivail@meinespielzeugkiste.de>
2017-07-25 21:15:17 +0200

    working training

--- small_cnn.py
+++ small_cnn.py
@@ -8,6 +8,10 @@ import numpy as np
 from scipy.signal import convolve2d
 from sklearn.datasets import load_digits
 
+#test function
+def delta_info(delta):
+    print('Mean '+str(np.mean(delta))+' Min: '+str(np.min(delta))+' MAx: '+str(np.max(delta)))
+
 #function to load MNIST dataset
 def load_mnist_data():
     data = load_digits(return_X_y=True)
@@ -24,6 +28,12 @@ def load_mnist_data():
         x_test[i] = im.reshape((8,8))
         y_test[i][data[1][i+1500]] = 1
 
+    x_train-=np.mean(x_train)
+    x_train/=np.std(x_train)
+    
+    x_test-=np.mean(x_test)
+    x_test/=np.std(x_test)
+    
     return x_train,y_train,x_test,y_test
     
 #############################################################
@@ -35,9 +45,9 @@ class small_cnn:
     def __init__(self):
         self.weights = []   
     
-    #derivative of RELU activation function
+    #derivative of (leaky) RELU activation function
     def _relu(self,x):
-        activate = lambda x: x if x>=0 else -0.01*x
+        activate = lambda x: x if x>=0 else 0.01*x
         relufunc = np.vectorize(activate)
         return relufunc(x)
         
@@ -66,24 +76,29 @@ class small_cnn:
   
     #Derivative of RELU activation function             
     def _d_relu(self,x):
-        activate = lambda x: 1 if x>=0 else -0.01
+        activate = lambda x: 1 if x>=0 else 0.01
         relufunc = np.vectorize(activate)
         return relufunc(x)       
     
     #Computes weight update for convolutional multi channels layers
-    def _compute_delta(self,d,y,fshape):
-        delta = np.zeros((16,3,3))
-        for c in range(fshape[0]):
-            for a in range(fshape[1]):
-                for b in range(fshape[2]):
+    def _compute_delta(self,d,y):
+        channel_size = 8
+        filter_size = 3
+        n_filter = 16
+        delta = np.zeros((n_filter,filter_size,filter_size))
+        #sum over current channel layers
+        for c in range(n_filter):
+            for a in range(filter_size):
+                for b in range(filter_size):
+                    #multi channel case for previous layer 
                     if len(y.shape)>2:
-                        for k in range(y.shape[0]):
-                            for i in range(y.shape[1]-fshape[1]):
-                                for j in range(y.shape[2]-fshape[2]):
+                        for k in range(n_filter):
+                            for i in range(channel_size-filter_size):
+                                for j in range(channel_size-filter_size):
                                     delta[c,a,b] +=d[c,i,j] *y[k,i+a,j+b]
                     else:
-                        for i in range(y.shape[0]-fshape[1]):
-                            for j in range(y.shape[1]-fshape[2]):
+                        for i in range(channel_size-filter_size):
+                            for j in range(channel_size-filter_size):
                                 delta[c,a,b] +=d[c,i,j] *y[i+a,j+b]
                         
         return delta
@@ -113,12 +128,10 @@ class small_cnn:
             x0 = self._conv2d(x,weights[0])
             y0 = self._relu(x0)
             s0 = self._d_relu(x0)
-            
             #conv2
             x1 = self._conv2d(y0,weights[1])
             y1 = self._relu(x1)
             s1 = self._d_relu(x1)
-            
             y1_flatten = y1.flatten()
             
             #dense
@@ -135,7 +148,7 @@ class small_cnn:
             
             #back propagation
             
-            d3 = y-y3
+            d3 = y3-y
             delta3[0] +=  np.dot(np.array([y2]).T,np.array([d3]))
             delta3[1]+=d3
             gamma2 = np.dot(d3,weights[3][0].T)
@@ -149,58 +162,77 @@ class small_cnn:
             gamma1 = gamma1_flatten.reshape(y1.shape)
                  
             d1 = s1*gamma1        
-            delta1 += self._compute_delta(d1,y0,weights[1].shape)
+            delta1 += self._compute_delta(d1,y0)
             gamma0 = np.sum([self._conv2d(d1[c],self._flip_filter(weights[1][c])) for c in range(weights[1].shape[0])])
             
             d0 = s0*gamma0
-            delta0 += self._compute_delta(d0,x,weights[0].shape)
+            delta0 += self._compute_delta(d0,x)
             
         #mean over batch    
         LOSS/=len(x_batch)
-        
+        #print(delta3[1][0])
         delta0/=len(x_batch)
         delta1/=len(x_batch)
         delta2[0]/=len(x_batch)
         delta2[1]/=len(x_batch)
         delta3[0]/=len(x_batch)
         delta3[1]/=len(x_batch)
+        #print(delta3[1][0])
+        #TEST
+        print('INFOS'+'>>'*10)
+        #delta_info(delta0)
+        #delta_info(delta1)
+        #delta_info(delta2[0])
+        #delta_info(delta2[1])
+        #delta_info(delta3[0])
+        print(model.predict(x_batch))
+        
         
         #weight update
-        weights[0]+=lrate*delta0
-        weights[1]+=lrate*delta1    
-        weights[2][0]+=lrate*delta2[0]
-        weights[2][1]+=lrate*delta2[1]
-        weights[3][0]+=lrate*delta3[0]
-        weights[3][1]+=lrate*delta3[1]
+        weights[0]  -= lrate*delta0
+        weights[1]  -= lrate*delta1    
+        weights[2][0]-= lrate*delta2[0]
+        weights[2][1]-= lrate*delta2[1]
+        weights[3][0]-= lrate*delta3[0]
+        weights[3][1]-= lrate*delta3[1]
+        delta_info(weights[3][0])
 
         return weights,LOSS    
 
        
 
     def initialize_weights(self):
-        alpha0 = 0.05
+        alpha0 = 0.5
         self.weights.append(alpha0*np.random.sample((16,3,3))-alpha0/2)
+        alpha0 = 0.5
         self.weights.append(alpha0*np.random.sample((16,3,3))-alpha0/2)
+        alpha0 = 0.5
         self.weights.append([alpha0*np.random.sample((1024,32))-0.5*alpha0,alpha0*np.random.sample(32)-0.5*alpha0])
+        alpha0 = 0.5
         self.weights.append([alpha0*np.random.sample((32,10))-0.5*alpha0,alpha0*np.random.sample(10)-0.5*alpha0])
 
     def train(self,data,labels,test_data,test_labels,initial_lrate=0.05,epochs = 50):
         
-        n_batch = int(len(data)/32)
+        BATCH_SIZE = 64
+        
+        n_batch = int(len(data)/BATCH_SIZE)
         lrate = initial_lrate
-        decay = initial_lrate/epochs
+        decay = initial_lrate/epochs       
+        
         
         for e in range(epochs):
             print('<>'*10)
             print('epoch '+ str(e)+'/'+str(epochs))
+            previous_loss = 100.
             for i in range(n_batch):
-                self.weights,LOSS =self._upgrade_network(data[32*i:32*(i+1)],labels[32*i:32*(i+1)],self.weights,lrate=lrate)
+                self.weights,LOSS =self._upgrade_network(data[BATCH_SIZE*i:BATCH_SIZE*(i+1)],labels[BATCH_SIZE*i:BATCH_SIZE*(i+1)],self.weights,lrate=lrate)
                 if LOSS>100:
                     print('Loss explosion')
                     break
+
             print('LOSS >> '+str(LOSS))
             if e%10==0:
-                self.save_weights('models/autosave-ep'+str(e))
+                self.save_weights('models/autosave2-ep'+str(e))
             self.evaluate(test_data,test_labels)
             lrate-=decay
         
@@ -252,11 +284,20 @@ if __name__=='__main__':
 
     
     data_train,label_train,data_test,label_test = load_mnist_data()
+    print(np.mean(data_train),np.mean(data_test))
+    print(np.std(data_train),np.std(data_test))
     
     model = small_cnn()
     model.initialize_weights()
-    model.train(data_train,label_train,data_test,label_test,initial_lrate=0.1,epochs=60)
-    model.save_weights('weights/model-')
+    model.train(data_train,label_train,data_test,label_test,initial_lrate=0.01,epochs=50)
+    
+    model.save_weights('weights/model2-')
+    
+    ypred = np.argmax(model.predict(data_test[0]))
+    
+    
+    
+    
     
     
     
